be3444dc635914bf10aee7c50a0b1ac66a57e31a
christianpinna <christian.a.pinna@gmail.com>
2022-12-08 18:47:34 +0000

    Added feed connection to Api to get all posts

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+import axios from "axios";
 
-const Container = styled.div``
-const Wrapper = styled.div``
+const Container = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const Post = styled.img``;
+
+const baseURL = "http://localhost:8000/api/post";
 
 export const Feed = () => {
+  const [posts, setPosts] = React.useState(null);
+
+  React.useEffect(() => {
+    axios.get(baseURL).then((res) => {
+      setPosts(res.data.data);
+    });
+  }, []);
+
+  const createPosts = (posts) => {
+    return posts.map((post) => <Post src={post.link} />);
+  };
+
   return (
-    <div>Feed</div>
-  )
-}
+    <Container>
+      <Wrapper>{posts ? createPosts(posts) : "No Posts"}</Wrapper>
+    </Container>
+  );
+};
