ff038c584ba65a53cd0b53d0fe4679500b7b48b5
christianpinna <christian.a.pinna@gmail.com>
2022-12-09 11:24:40 +0000

    Create component connected to API createPost

--- client/src/components/Create.jsx
+++ client/src/components/Create.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import React, {useState} from 'react'
 import styled from 'styled-components'
 
@@ -7,33 +8,52 @@ const Container = styled.div`
 const Wrapper = styled.div`
 display: flex;
 flex-direction: column;
+padding: 1rem;
 `
 const CreateForm = styled.form`
 display: flex;
 flex-direction: column;
-width: 5rem;
+height: 20rem;
+width: 30rem;
 `
 const LinkInput = styled.input`
+width: 20 rem;
+margin: 1rem;
+`
+const CreateButton = styled.button`
+  background-color: #f4094f;
+  margin: 1rem;
+
 `
 const ImgPreview = styled.img`
   width: 50rem;
   height: auto;
+  margin: 1rem;
+
 `
 
 export const Create = () => {
-  const [file, setFile] = useState();
+  const [link, setLink] = useState();
 
   const handleChange = (e)=>{
     console.log(e.target.value)
-    setFile(e.target.value)
+    setLink(e.target.value)
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:8000/api/post", {link: link} ).then((res) => {
+      console.log(res.data.message);
+    });
   }
 
   return (
     <Container>
       <Wrapper>
-        <ImgPreview src={file} />
-        <CreateForm>
-          <LinkInput type="text" onChange={handleChange}></LinkInput>
+        <h1>Create a Post!</h1>
+        <ImgPreview src={link} />
+        <CreateForm onSubmit={handleSubmit}>
+          <LinkInput type="text" onChange={handleChange} placeholder="Add you link"></LinkInput>
+          <CreateButton type='submit'>Create</CreateButton>
         </CreateForm>
       </Wrapper>
     </Container>
